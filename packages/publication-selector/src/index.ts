51544cf67b37e6e1fcee4fb2a90c15ad04dede7d
atrofimova-blackbird <atrofimova@blackbird-lab.com>
2021-01-26 14:20:25 +0200

    update to p.5

--- packages/publication-selector/src/index.ts
+++ packages/publication-selector/src/index.ts
@@ -1,3 +1,3 @@
-const publicationSelector = "======= Publication selector component. Test update to p.4 =======";
+const publicationSelector = "======= Publication selector component. Test update to p.5 =======";
 
 export default publicationSelector;
